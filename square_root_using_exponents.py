27c5f26362b45b04cd8a4ed3aff1bbf363da0450
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-12-03 20:35:22 +0100

    Create square_root_using_exponents.py

---
+++ square_root_using_exponents.py
@@ -0,0 +1,10 @@
+ # square_root() use exponents (**) to return the square root of num
+ 
+ def square_root(num):
+  return num ** .5
+
+# Uncomment these function calls to test your square_root function:
+print(square_root(16))
+# should print 4
+print(square_root(100))
+# should print 10
