0a96367a3473bca3f905a73deb30725ffc8e9035
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2020-12-17 11:10:04 +0100

    Fixing author bio w/.strip(), .find() & .replace()

--- toomer_bio_fixed.py
+++ toomer_bio_fixed.py
@@ -23,3 +23,18 @@ print(disown_placement)
 # https://www.poetryfoundation.org/poetrymagazine/browse?contentId=23104
 
 # You can also search for larger strings, and .find() will return the index value of the first character of that string. 
+
+
+
+# .strip(). Stripping a string removes all whitespace characters from the beginning and end. 
+
+love_maybe_lines = ['Always    ', '     in the middle of our bloodiest battles  ', 'you lay down your arms', '           like flowering mines    ','\n' ,'   to conquer me home.    ']
+
+love_maybe_lines_stripped = []
+
+for line in love_maybe_lines:
+  love_maybe_lines_stripped.append(line.strip())
+  
+love_maybe_full = '\n'.join(love_maybe_lines_stripped)
+
+print(love_maybe_full)
