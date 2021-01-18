afcec667c3b2ec864457463aefd93ff8e805a1d7
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-01-18 12:17:14 +0100

    Create alphabet.py

---
+++ alphabet.py
@@ -0,0 +1,40 @@
+# Candy Tale 55  
+## Fun Fact: I love weird books.
+
+
+
+
+##   VOWELS   ## 
+
+a = [
+"   A    ",
+"  A A   ",
+" A   A  ",
+"A A A A ",
+"A     A ",
+"A     A ",
+"A     A "]
+
+
+##   CONSONANTS   ## 
+
+b = [
+"BBBBB  ",
+"B    B ",
+"B    B ",
+"BBBBB  ",
+"B    B ",
+"B    B ",
+"BBBBB  "
+]
+
+
+s = [
+"  SSS  ",
+" S   S ",
+" S     ,
+"  SSS  ,
+"     S ",
+" S   S ",
+"  SSS  "
+]
