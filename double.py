87db7ba4d58d1d3dbfe22b4e7a69dfcfc0e49370
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-01-29 17:19:31 +0100

    multiplying each number by two
    Create a new list named double_nums by multiplying each number in nums by two.

---
+++ double.py
@@ -0,0 +1,7 @@
+# Create a new list named double_nums by multiplying each number in nums by two.
+
+nums = [4, 8, 15, 16, 23, 42]
+double_nums = [x * 2 for x in nums]
+
+print(double_nums)
+# [8, 16, 30, 32, 46, 84]
