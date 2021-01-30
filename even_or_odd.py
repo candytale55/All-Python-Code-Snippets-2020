188f3e36fd36a8ec04ddd8d4056e5475af8401e6
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-01-30 12:10:58 +0100

    return "even" or "odd" if num is even or odd

---
+++ even_or_odd.py
@@ -0,0 +1,6 @@
+# even_or_odd takes an integer named num. If num is even, return "even". If num is odd, return "odd".
+
+even_or_odd = lambda num : "even" if num % 2 == 0 else "odd" 
+
+print even_or_odd(10) # even
+print even_or_odd(5)  # odd
