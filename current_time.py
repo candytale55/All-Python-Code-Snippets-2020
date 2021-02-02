74c9b0680e6a8f6ef837e5ff606bf0d92e6237a5
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-02 19:49:57 +0100

    Use of datetime.now()

---
+++ current_time.py
@@ -0,0 +1,25 @@
+# One common library that comes as part of the Python Standard Library is datetime. datetime helps you work with dates and times in Python.
+
+# Datetime is both the name of the library and the name of the object that you are importing.
+
+
+
+from datetime import datetime
+
+current_time = datetime.now()
+print(current_time)
+
+
+
+# Python allows us to package code into files or sets of files called modules.
+
+# A module is a collection of Python declarations intended broadly to be used as a tool. Modules are also often referred to as “libraries” or “packages” — a package is really a directory that holds a collection of modules.
+
+# from module_name import object_name
+
+# A library will include a lot of code that you don’t need that may slow down your program or conflict with existing code. Because of this, it makes sense to only import what you need.
+
+
+# https://discuss.codecademy.com/t/how-can-we-see-all-the-objects-and-functions-in-a-module/374436
+
+# https://discuss.codecademy.com/t/why-doesnt-datetime-now-match-with-the-current-time/460842
