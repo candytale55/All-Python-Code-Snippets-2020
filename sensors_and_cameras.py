b9c6b37564220a9b417f15f9e86d25f23ba381a2
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-06 11:12:22 +0100

    examples - printing dictionaries

---
+++ sensors_and_cameras.py
@@ -0,0 +1,20 @@
+# A dictionary is an unordered set of key: value pairs. 
+
+
+# EXAMPLES: 
+# You have a dictionary of temperature sensors in your house and what temperatures they read. You’ve just added a sensor to your "pantry", and it reads 22 degrees.
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+
+# The dictionary num_cameras represents the number of cameras in each area around your house.
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+# {'living room': 21, 'kitchen': 23, 'bedroom': 20, 'pantry': 22}
+
+print(num_cameras)
+# {'backyard': 6, 'garage': 2, 'driveway': 1}
+
+
+
+# Reference: https://discuss.codecademy.com/t/why-arent-dictionaries-printed-in-the-same-order-as-they-are-created/461685
