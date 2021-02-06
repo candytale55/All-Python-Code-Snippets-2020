b60d6c28bdbac89fe2ccc38edfb325edc390e8aa
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-06 11:20:18 +0100

    example of dictionaries - keys and values.

--- sensors_and_cameras.py
+++ sensors_and_cameras.py
@@ -1,8 +1,7 @@
-# A dictionary is an unordered set of key: value pairs. 
+# A dictionary is an unordered set of key: value pairs. EXAMPLES: 
 
 
-# EXAMPLES: 
-# You have a dictionary of temperature sensors in your house and what temperatures they read. You’ve just added a sensor to your "pantry", and it reads 22 degrees.
+# You have a dictionary of temperature sensors in your house and what temperatures they read. 
 
 sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
 
@@ -14,6 +13,20 @@ print(sensors)
 
 print(num_cameras)
 # {'backyard': 6, 'garage': 2, 'driveway': 1}
+
+
+
+# keys can be numbers as well. For example, mapping restaurant bill subtotals to the bill total after tip: 
+subtotal_to_total = {20: 24, 10: 12, 5: 6, 15: 18}
+
+# Values can be any type: a string, a number, a list, or even another dictionary: 
+students_in_classes = {"software design": ["Aaron", "Delila", "Samson"], "cartography": ["Christopher", "Juan", "Marco"], "philosophy": ["Frederica", "Manuel"]}
+print (students_in_classes["software design"])
+# ['Aaron', 'Delila', 'Samson']
+
+# You can also mix and match key and value types:
+
+person = {"name": "Shuri", "age": 18, "siblings": ["T'Chaka", "Ramonda"]}
 
 
 
