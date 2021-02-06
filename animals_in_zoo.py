5c7f2b42459e524caf8a75f8ab10c65002d23e3e
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-06 11:30:29 +0100

    emtpy dictionaries and adding key:values

---
+++ animals_in_zoo.py
@@ -0,0 +1,29 @@
+# We can create an empty dictionary when we plan to fill it later based on some other input.
+
+my_empty_dictionary = {}
+
+# REF: https://discuss.codecademy.com/t/how-can-i-determine-if-a-dictionary-is-empty/352838
+
+
+my_dict = {}
+# To add a single key : value pair to a dictionary, we can use the syntax:
+my_dict["new_key"] = "new_value"
+print(my_dict)
+
+
+
+menu = {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+# If we want to add a new item, "cheesecake" for 8 dollars, we could use:
+menu["cheesecake"] = 8
+print(menu)
+
+
+
+animals_in_zoo = {} # Empty list
+animals_in_zoo["zebras"] = 8 
+animals_in_zoo["monkeys"] = 12
+animals_in_zoo["dinosaurs"]= 0
+print(animals_in_zoo)
+
+
+# Reference: https://discuss.codecademy.com/t/can-an-empty-key-be-used-to-insert-a-value-into-a-dictionary/352848
