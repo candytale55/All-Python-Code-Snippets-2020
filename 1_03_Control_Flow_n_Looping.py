7ba7a652a4d9c993c776819b9a9c05cecbca96c8
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-18 12:45:13 +0100

    for loop in a list, prints number if even
    # loop through each item in the list called a, if the number is even, print it out. If the item % 2 == 0.
    
    a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
    
    for item in a:
      if item % 2 == 0:
        print item

---
+++ 1_03_Control_Flow_n_Looping.py
@@ -0,0 +1,23 @@
+# Python 2
+# loop through each item in the list called a, if the number is even, print it out. If the item % 2 == 0.
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for item in a:
+  if item % 2 == 0:
+    print item
+
+# Prints: 
+"""
+0
+2
+4
+6
+8
+10
+12
+"""
+
+
+
+# REF: https://discuss.codecademy.com/t/why-does-my-code-not-print-values-divisible-by-2/339195
