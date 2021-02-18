ac90d5a03dd80ba6710903bbb361155cc2f30f54
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-02-18 12:34:41 +0100

    for in syntax to print items of a list
    # The for loop in terms of how it relates to lists: It allow us to iterate through all of the elements 
    # in a list from the left-most (or zeroth element) to the right-most element.
    
    #  be careful to avoid using the word list as a variable, since that’s a reserved word
    
    
    
    names = ["Adam","Alex","Mariah","Martine","Columbus"]
    
    for name in names:
      print name

---
+++ 1_01_BeFOR_We_Begin.py
@@ -0,0 +1,24 @@
+# The for loop in terms of how it relates to lists: It allow us to iterate through all of the elements 
+# in a list from the left-most (or zeroth element) to the right-most element.
+
+#  be careful to avoid using the word list as a variable, since that’s a reserved word
+
+
+
+names = ["Adam","Alex","Mariah","Martine","Columbus"]
+
+for name in names:
+  print name
+
+
+# Output
+"""
+Adam
+Alex
+Mariah
+Martine
+Columbus
+"""
+
+
+# REF: https://discuss.codecademy.com/t/why-does-it-print-the-entire-list-each-time-my-loop-runs/339179
