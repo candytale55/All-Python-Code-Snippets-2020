eb03079add6bee2802809a619381b90f10b1dca6
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-03-01 21:38:04 +0100

    Create 02_Class_Animal__Class_Syntax.py

---
+++ 02_Class_Animal__Class_Syntax.py
@@ -0,0 +1,19 @@
+# A basic class consists only of the class keyword, the name of the class, and the class from which the new class inherits in parentheses.  
+# For now, our classes will inherit from the _object_ class. 
+
+# This gives them the powers and abilities of a Python object. By convention, user-defined Python class names start with a capital letter.
+
+
+class NewClass(object):
+  # Class magic here
+  pass
+
+
+
+class Animal(object):
+  pass
+
+# pass doesn’t do anything, but it’s useful as a placeholder in areas of your code where Python expects an expression.
+
+
+# https://discuss.codecademy.com/t/what-does-it-mean-to-inherit-from-the-object-class/340587
