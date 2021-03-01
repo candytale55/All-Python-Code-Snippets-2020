1ae1c7ac0c6ab3f8e144b206627e29cdbfe140ab
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-03-01 22:02:21 +0100

    Update 02_Class_Animal__Class_Syntax.py

--- 02_Class_Animal__Class_Syntax.py
+++ 02_Class_Animal__Class_Syntax.py
@@ -24,9 +24,17 @@ class NewClass(object):
 
 
 class Animal(object):
-  def __init__(self):
-    pass
+  def __init__(self, name):
+    self.name = name  #  let the function know that name refers to the created object’s name by typing self.name = name
 
+
+"""
+ Python will use the first parameter that __init__() receives to refer to the object being created; 
+ this is why it’s often called self, since this parameter gives the object being created its identity. 
+ This is a Python convention; there’s nothing magic about the word self but it’s overwhelmingly common 
+ to use self as the first parameter in __init__(), so you should use this so that other people will understand your code.
+"""
 
 # https://discuss.codecademy.com/t/what-does-it-mean-to-inherit-from-the-object-class/340587
 # https://discuss.codecademy.com/t/what-is-the-difference-between-a-class-and-an-object/340593
+# https://discuss.codecademy.com/t/what-is-self/340594
