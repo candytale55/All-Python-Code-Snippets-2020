79ac23fce2e9697ec0cf36a7185ee17dcfe2439a
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-03-01 21:47:42 +0100

    Update 02_Class_Animal__Class_Syntax.py

--- 02_Class_Animal__Class_Syntax.py
+++ 02_Class_Animal__Class_Syntax.py
@@ -8,12 +8,25 @@ class NewClass(object):
   # Class magic here
   pass
 
-
-
-class Animal(object):
-  pass
-
 # pass doesn’t do anything, but it’s useful as a placeholder in areas of your code where Python expects an expression.
 
 
+
+class NewClass(object):
+  # Class magic here
+  pass
+
+
+# __init__()
+#  we start our class definition off with an odd-looking function: __init__(), required for classes, and it’s used to initialize the objects it creates. 
+
+# __init__() always takes at least one argument, self, that refers to the object being created. You can think of __init__() as the function that “boots up” each object the class creates.
+
+
+class Animal(object):
+  def __init__(self):
+    pass
+
+
 # https://discuss.codecademy.com/t/what-does-it-mean-to-inherit-from-the-object-class/340587
+# https://discuss.codecademy.com/t/what-is-the-difference-between-a-class-and-an-object/340593
