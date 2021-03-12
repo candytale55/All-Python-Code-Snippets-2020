ec5cb04b5d812c8a1f4830c3d754dff70a4566ae
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-03-12 11:05:22 +0100

    binary numbers all the way up to twelve

---
+++ 03_Count_to_1100.py
@@ -0,0 +1,17 @@
+# fill out the rest of the numbers all the way up to twelve
+
+one = 0b1
+two = 0b10
+three = 0b11
+four  = 0b100 
+five  = 0b101
+six   = 0b110
+seven = 0b111
+eight = 0b1000
+nine  = 0b1001
+ten   = 0b1010
+eleven = 0b1011
+twelve = 0b1100
+
+
+# https://discuss.codecademy.com/t/how-can-i-write-the-binary-equivalent-of-a-number/340573
