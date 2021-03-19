e5ccd4532c5926fdb6b189413f9a5779b7d152a6
CandyTale55 <53194028+candytale55@users.noreply.github.com>
2021-03-19 02:07:17 +0100

    bitwise NOT operator ~ flips all bits in a number

---
+++ 10_This_is_NOT_That_Hard.py
@@ -0,0 +1,24 @@
+"""
+The bitwise NOT operator (~) just flips all of the bits in a single number.  
+What's done inside the computer is very complicated, but mathematically, 
+this is equivalent to adding one to the number and then making it negative.
+"""
+
+print ~1
+print ~2
+print ~3
+print ~42
+print ~123
+
+
+"""
+OUTPUT: 
+
+-2
+-3
+-4
+-43
+-124
+"""
+
+# https://discuss.codecademy.com/t/why-would-we-want-to-change-bit-strings-with-bitwise-operators/340579
